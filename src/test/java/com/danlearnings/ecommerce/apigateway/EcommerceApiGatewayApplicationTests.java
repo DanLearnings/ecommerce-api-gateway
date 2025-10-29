cf5a6b5b4f4881386d2bda11277874bc80daa0af
DanrleyBrasil <danrleybrasil.s@gmail.com>
2025-10-28 21:40:04 -0300

    Initial commit: API Gateway setup with routes

---
+++ src/test/java/com/danlearnings/ecommerce/apigateway/EcommerceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danlearnings.ecommerce.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
